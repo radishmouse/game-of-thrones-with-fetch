92a01a319960d94e626caf37de03cbec580ea55f
Chris Aquino <chris.aquino@gmail.com>
2019-03-12 11:36:15 -0400

    sorts characters and draws them to listing

--- scripts/index.js
+++ scripts/index.js
@@ -56,13 +56,54 @@ function retrievePageOfCharacters(pageNumber) {
       })    
 }
 
+function drawSingleCharacterToListing(characterObject) {
+    
+    const characterName = characterObject.name;
+    if (characterName.length === 0) {
+        return;
+    }
+
+    const anchorElement = document.createElement('a');
+    anchorElement.textContent = characterName;
+
+    const listItem = document.createElement('li');
+    listItem.appendChild(anchorElement);
+
+    const listArea = document.querySelector('[data-listing]');
+
+    listArea.appendChild(listItem);
+}
+
+function drawListOfCharacters() {
+    // uses global variable allCharactersArray
+    
+    // loop through the array of characters
+    // for each one, draw the name in the listing
+    // area of the page.
+    allCharactersArray.forEach(drawSingleCharacterToListing);
+}
+
+function sortByName(obj1, obj2) {
+    const letter1 = obj1.name[0];
+    const letter2 = obj2.name[0];
+
+    if (letter1 < letter2) {
+        return -1;
+    } else if (letter2 < letter1) {
+        return 1;
+    }
+
+    return 0;
+}
+
 
 function main() {    
     let charactersInLocalStorage = loadCharacters();
     if (charactersInLocalStorage) {
         allCharactersArray = [
-            ...charactersInLocalStorage
+            ...charactersInLocalStorage.sort(sortByName)
         ];
+        drawListOfCharacters();
     } else {
         console.log("You got a whole lotta nuthin'.");
         console.log("Retrieving from the API");
